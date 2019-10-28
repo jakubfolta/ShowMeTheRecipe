fc7a0fa5a840f42449f3f8aa49e147abf0320815
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-10-28 22:44:47 +0000

    Continue building search controller.

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,11 @@
 import Search from './models/Search';
 
+/** Global state of the app
+* - Search object
+* - Current recipe object
+* - Shopping list object
+* - Liked recipes
+*/
+
 const search = new Search('pizza');
 search.getResults();
