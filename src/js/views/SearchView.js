3af5d26550fd5e8e07d64db850f430cf2ea459ff
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-10-31 23:26:04 +0000

    Build search view part 2 - continue - pagination.

--- src/js/views/SearchView.js
+++ src/js/views/SearchView.js
@@ -43,6 +43,19 @@ const renderRecipe = recipe => {
     elements.searchResList.insertAdjacentHTML('beforeend', markup);
 };
 
+const renderButtons = (page, numResults, resPerPage) => {
+    const pages = Math.ceil(numResults / resPerPage);
+
+    if (page === 1 && pages > 1) {
+        // Only button to next page
+    } else if (page < pages) {
+        // Both buttons
+    } else if (page === pages && pages > 1) {
+        // Only button to previous page
+    }
+
+}
+
 export const renderResults = (recipes, page = 1, resPerPage = 10) => {
     const start = (page - 1) * resPerPage;
     const end = page * resPerPage;
