3a4aa94a75f2de95fb71f851df258372f320149e
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-11-01 23:45:30 +0000

    Build search view part 2 - continue - pagination.

--- src/js/views/SearchView.js
+++ src/js/views/SearchView.js
@@ -76,9 +76,34 @@ const renderButtons = (page, numResults, resPerPage) => {
 };
 
 export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+    // Render results of current page
     const start = (page - 1) * resPerPage;
     const end = page * resPerPage;
 
     recipes.slice(start, end).forEach(renderRecipe);
+
+    // Render pagination
     renderButtons(page, recipes.length, resPerPage)
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
