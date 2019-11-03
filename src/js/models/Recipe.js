f230edfb7a40fd94b5fc8ecc7a14ebe440b322a6
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-11-03 11:45:45 +0000

    Build recipe model - parseIngredients.

--- src/js/models/Recipe.js
+++ src/js/models/Recipe.js
@@ -33,6 +33,14 @@ export default class Recipe {
 
     parseIngredients() {
         const newIngredients = this.ingredients.map(el => {
+            // 1. Uniform units
+
+
+            // 2. Remove parentheses
+
+
+            // 3. Parse ingredients into count, unit and ingredient
+
             
         })
         this.ingredients = newIngredients;
