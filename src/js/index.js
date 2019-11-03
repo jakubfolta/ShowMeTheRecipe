f3fd81244f3fd55efffbdb8b3cc268e62f1659ab
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-11-03 01:06:25 +0000

    Build recipe controller - controlRecipe.

--- src/js/index.js
+++ src/js/index.js
@@ -57,32 +57,34 @@ elements.searchResPages.addEventListener('click', e => {
 * RECIPE CONTROLLER
 */
 
-const controlRecipe = () => {
+const controlRecipe = async () => {
     // Get ID from the url
     const id = window.location.hash.replace('#', '');
-    console.log(id);
 
     if (id) {
         // Prepare UI for changes
         renderLoader(elements.searchRec);
 
         // Create new recipe object
-        const newRecipe = new Recipe(id);
-        newRecipe.getRecipe();
-        console.log(newRecipe);
+        state.recipe = new Recipe(id);
 
         // Get recipe data
+        await state.recipe.getRecipe();
 
 
         // Calculate servings and time
-
+        state.recipe.calcTime();
+        state.recipe.calcServings();
 
         // Render recipe
+        console.log(state.recipe);
     }
 };
 
-window.addEventListener('hashchange', controlRecipe);
+// window.addEventListener('hashchange', controlRecipe);
+// window.addEventListener('load', controlRecipe);
 
+['hashchange', 'load'].forEach( event => window.addEventListener(event, controlRecipe))
 
 
 
