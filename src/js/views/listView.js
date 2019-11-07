a9d7a03da702d5e6b6bc0cd129291d1fcc5a7e8f
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-11-07 07:25:48 +0000

    Build shopping list view.

--- src/js/views/listView.js
+++ src/js/views/listView.js
@@ -2,12 +2,12 @@ import {elements} from './base';
 
 export const renderItem = item => {
     const markup = `
-        <li class="shopping__item" data-itemid>
+        <li class="shopping__item" data-itemid=${item.id}>
             <div class="shopping__count">
-                <input type="number" value="500" step="100">
-                <p>g</p>
+                <input type="number" value="${item.count}" step="100">
+                <p>${item.unit}</p>
             </div>
-            <p class="shopping__description">Pasta</p>
+            <p class="shopping__description">${item.ingredient}</p>
             <button class="shopping__delete btn-tiny">
                 <svg>
                     <use href="img/icons.svg#icon-circle-with-cross"></use>
