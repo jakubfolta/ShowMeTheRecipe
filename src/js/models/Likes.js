1ea9d50ecfc74b448674b14625a5f495a1cf20c3
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-11-08 23:54:07 +0000

    Build likes model.

--- src/js/models/Likes.js
+++ src/js/models/Likes.js
@@ -4,8 +4,14 @@ export default class Likes {
     }
 
     addLike(id, title, author, img) {
-        const like = {
-            this.likes.push(id, title, author, img)
-        }
+        const like = { id, title, author, img };
+        this.likes.push(like);
+        return like;
+    }
+
+    deleteLike(id) {
+        const index = this.likes.findIndex( el => el.id === id);
+
+        this.likes.splice(index, 1);
     }
 }
